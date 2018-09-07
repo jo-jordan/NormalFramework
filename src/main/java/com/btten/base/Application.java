193809de2f1def1617a4619c56d9ec199d7c4594
lzjlxebr <tyyzmyd@gmail.com>
2018-09-07 17:31:07 +0800

    更新包名扫描

--- src/main/java/com/btten/base/Application.java
+++ src/main/java/com/btten/base/Application.java
@@ -4,9 +4,10 @@ import com.btten.action.test.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = HelloController.class)
+@ComponentScan({"com.btten.action"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
